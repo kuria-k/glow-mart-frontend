7d20ec7d88412902bd27460f395c006bd61d0765
kuria-k <kelvinmacharia442@gmail.com>
2026-05-05 12:42:30 +0300

    api.js

--- src/api.js
+++ src/api.js
@@ -28,7 +28,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 // Request interceptor - ADD JWT TOKEN
